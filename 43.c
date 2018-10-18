0f47e997fe9b845ca0dc17d8155092ac362a4506
a l C o P a U L <paul.amurao@gmail.com>
2018-10-18 16:36:38 -0400

    Update 43.c

--- 43.c
+++ 43.c
@@ -7,19 +7,19 @@ int main()
 	char fff[512];
 	char *h = "Enter text: ";
 	int matrix[4][4] = {
-		{4,15,17,7},
-		{5,19,13,6},
-		{20,9,2,12},
-		{14,0,11,18}};
+		{ 4, 15, 17, 7 },
+		{ 5, 19, 13, 6 },
+		{ 20, 9, 2, 12 },
+		{ 14, 0, 11, 18 } };
 	int hh = 0;
 	printf("%s", h);
-	gets_s(fff,sizeof(fff));
-	int k = strlen(fff);
+	gets_s(fff, sizeof(fff));
+	int k = (int) strlen(fff);
 	for (int u = 0; u < k; ++u)
 	{
-		hh += (int) fff[u];
+		hh += (int)fff[u];
 	}
-	srand(43*hh);
+	srand(43 * hh);
 	int buff;
 	int total = 0;
 	for (int i = 0; i < 4; ++i)
@@ -28,7 +28,7 @@ int main()
 		{
 			buff = matrix[i][y] - matrix[rand() % 4][rand() % 4];
 			total += buff;
-			
+
 		}
 	}
 	printf("%d", total);
